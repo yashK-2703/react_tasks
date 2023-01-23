4d7c12048e8113d51db8d7c18a98abe116f8a733
yashK-2703 <yashkedia274@gmail.com>
2023-01-23 18:59:03 +0530

    Fix #1: indentation

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,17 +1,16 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState } from "react";
 
 class TodoApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [],
-      currentTodo: '',
-      priority: 'normal',
-      searchTerm: '',
+      currentTodo: "",
+      priority: "normal",
+      searchTerm: "",
       filteredTodos: [],
-      isSearchEnabled: false
+      isSearchEnabled: false,
     };
-    
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
@@ -24,7 +23,7 @@ class TodoApp extends Component {
   handleChange(e) {
     this.setState({
       currentTodo: e.target.value,
-      searchTerm: ''
+      searchTerm: "",
     });
   }
 
@@ -33,86 +32,102 @@ class TodoApp extends Component {
     const newTodo = {
       id: Date.now(),
       text: this.state.currentTodo,
-      priority: this.state.priority
+      priority: this.state.priority,
     };
     this.setState({
       todos: [...this.state.todos, newTodo],
-      currentTodo: '',
-      priority: 'normal',
-      isSearchEnabled: false
+      currentTodo: "",
+      priority: "normal",
+      isSearchEnabled: false,
     });
   }
 
   handleSearchChange(e) {
     this.setState({
-      searchTerm: e.target.value
+      searchTerm: e.target.value,
     });
   }
-  
+
   handleSearch(e) {
     e.preventDefault();
-    let filteredTodos = this.state.todos.filter(todo => todo.text.includes(this.state.searchTerm));
+    let filteredTodos = this.state.todos.filter((todo) =>
+      todo.text.includes(this.state.searchTerm)
+    );
     this.setState({
       filteredTodos: filteredTodos,
-      isSearchEnabled: true
+      isSearchEnabled: true,
     });
   }
 
   removeTodo(id) {
     this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id),
-      filteredTodos: this.state.todos
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+      filteredTodos: this.state.todos,
     });
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit} style={{paddingBottom: '20px'}}>
+        <form onSubmit={this.handleSubmit} style={{ paddingBottom: "20px" }}>
           <input
             type="text"
             value={this.state.currentTodo}
             onChange={this.handleChange}
             placeholder="Enter a new todo"
           />
-          <select id="priority" value={this.state.priority} onChange={this.handlePriorityChange}>
-             <option value="normal">Normal</option>
-             <option value="high">High</option>
-             <option value="low">Low</option>
+          <select
+            id="priority"
+            value={this.state.priority}
+            onChange={this.handlePriorityChange}
+          >
+            <option value="normal">Normal</option>
+            <option value="high">High</option>
+            <option value="low">Low</option>
           </select>
           <button type="submit">Add Todo</button>
         </form>
 
         <form>
-          <input type="text" value={this.state.searchTerm} onChange={this.handleSearchChange} placeholder="Search Todo"/>
+          <input
+            type="text"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchChange}
+            placeholder="Search Todo"
+          />
           <button onClick={this.handleSearch}>Search</button>
         </form>
-        {this.state.isSearchEnabled?
-          (
+        {this.state.isSearchEnabled ? (
           <ul>
-          {this.state.filteredTodos.map(todo => (
-            <li key={todo.id} style={{color: this.handleColor(todo.priority)}}>
-              {todo.text}
-              <button onClick={() => this.removeTodo(todo.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>)
-        :
-        (
+            {this.state.filteredTodos.map((todo) => (
+              <li
+                key={todo.id}
+                style={{ color: this.handleColor(todo.priority) }}
+              >
+                {todo.text}
+                <button onClick={() => this.removeTodo(todo.id)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        ) : (
           <ul>
-          {this.state.todos.map(todo => (
-            <li key={todo.id} style={{color: this.handleColor(todo.priority)}}>
-              {todo.text}
-              <button onClick={() => this.removeTodo(todo.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>)}
+            {this.state.todos.map((todo) => (
+              <li
+                key={todo.id}
+                style={{ color: this.handleColor(todo.priority) }}
+              >
+                {todo.text}
+                <button onClick={() => this.removeTodo(todo.id)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
 
   handlePriorityChange(event) {
-    this.setState({priority: event.target.value});
+    this.setState({ priority: event.target.value });
   }
 
   handleColor(priority) {
